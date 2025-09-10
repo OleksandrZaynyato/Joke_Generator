e4a323b232d88e0336a04c5e18338c3bb939959a
OleksandrZaynyato <153942558+OleksandrZaynyato@users.noreply.github.com>
2025-09-10 19:17:27 +0300

    Made server structure

--- server/app.js
+++ server/app.js
@@ -1,22 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
 
-const express = require('express');
-const mongoose = require('mongoose');
-
+import connectDB from './config/DB.js';
 
 const app = express();
+
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb+srv://admin:creator12341234@cluster0.4gzdple.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => console.log('DB connected'))
+// Connect to MongoDB
+connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 
 app.get('/jokes', (req, res) => {
   res.send('all jokes');
 })
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
