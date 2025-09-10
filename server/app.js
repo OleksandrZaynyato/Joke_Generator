e16fa34f463b5685139e5d76d971f0abfca882c1
index22 <116450399+Svjatoslav22@users.noreply.github.com>
2025-09-10 17:05:49 +0300

    DB init

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect('mongodb+srv://admin:creator12341234@cluster0.4gzdple.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('DB connected'))
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
