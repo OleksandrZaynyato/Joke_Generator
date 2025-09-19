ee11ea30377664ee04cf0b5b35adf23762d7d9d0
OleksandrZaynyato <153942558+OleksandrZaynyato@users.noreply.github.com>
2025-09-19 17:37:43 +0300

    feat: add api url in .env

--- src/admin/authProvider/login.js
+++ src/admin/authProvider/login.js
@@ -1,7 +1,7 @@
 import { fetchUtils } from 'react-admin';
 
 export const login = ({ username, password }) => {
-    const API_URL = 'http://localhost:3000/api';
+    const API_URL = import.meta.env.VITE_API_URL;
 
     return fetchUtils.fetchJson(`${API_URL}/user/login`, {
         method: 'POST',
